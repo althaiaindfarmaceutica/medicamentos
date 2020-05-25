305089663e5ba457a788fd807f0e8fe258057572
Gabrinne <gabrinneloyse@hotmail.com>
2020-05-25 17:55:49 -0300

    adc v-model para databinding

--- formulario/src/App.vue
+++ formulario/src/App.vue
@@ -17,59 +17,72 @@
 
           <h3 class="mt-2">Preencha abaixo</h3>
 
-          <form>
+          <form @submit.prevent="enviar" @reset="resetar">
             
+           
             <div class="form-group">
-              <label>Apresentação</label>
-              <textarea class="form-control" placeholder="Apresentação do medicamento"></textarea>
+              <label>Apresentação do medicamento</label>
+               <input type="text" 
+                class="form-control"
+                placeholder="Informe o medicamento"
+                v-model.lazy.trim="$v.medicamentos.apresentacao.$model" >
             </div>
 
-             <div class="form-group">
+            <div class="form-group">
               <label>Descrição</label>
-              <textarea class="form-control" placeholder="Descrição do medicamento"></textarea>
+              <textarea class="form-control" 
+              placeholder="Descrição do medicamento" 
+              v-model.lazy.trim="$v.medicamentos.descricao.$model">
+              </textarea>
             </div>
 
             <div class="form-group">
               <label>Princípio ativo</label>
-              <input type="text" class="form-control" placeholder="Informe o nome do princípio ativo">
+               <input type="text" 
+                class="form-control"
+                placeholder="Informe o nome do princípio ativo"
+                v-model.lazy.trim="$v.medicamentos.principio.$model" >
             </div>
 
             
             <div class="form-group">
               <label>Empresa</label>
-              <input type="text" class="form-control" placeholder="Informe a empresa fabricante">
+              <input type="text"
+              class="form-control"
+              placeholder="Informe a empresa fabricante"
+              v-model.lazy.trim="$v.medicamentos.empresa.$model">
             </div>
 
 
-            <button class="btn btn-secondary">Resetar</button>
-            <button class="btn btn-success">Enviar</button>
+            <button class="btn btn-secondary" type="reset">Resetar</button>
+            <button class="btn btn-success" type="submit" >Enviar</button>
 
           </form>
 
         </div>
 
         <!-- saida -->
-        <div class="col-sm-6">
+        <div class="col-sm-6 ">
 
           <h3 class="mb-5">Saída</h3>
 
           <div class="card">
 
-            <div class="card-header">Dados preenchidos</div>
+            <div class="card-header">Dados preenchidos  </div>
 
             <ul class="list-group list-group-flush">
-              <li class="list-group-item"><strong>Apresentação:</strong></li>
-              <li class="list-group-item"><strong>Descrição:</strong> </li>
-              <li class="list-group-item"><strong>Princípio Ativo:</strong></li>
-              <li class="list-group-item"><strong>Empresa:</strong> </li>
+              <li class="list-group-item"><strong>Apresentação:</strong>  {{ medicamentos.apresentacao}}  </li>
+              <li class="list-group-item"><strong>Descrição:</strong> <pre> {{ medicamentos.descricao}} </pre> </li>
+              <li class="list-group-item"><strong>Princípio Ativo:</strong> {{ medicamentos.principio}}</li>
+              <li class="list-group-item"><strong>Empresa:</strong> {{ medicamentos.empresa}}</li>
             </ul>
 
 
-            <!-- 
+             
             <div class="card-header">Model</div>
               <div class="card-body">
-              <pre><code>{{ {'nome': 'Plínio Naves'} }}</code></pre>
-            </div> -->
+              <pre><code>{{ medicamentos }}</code></pre>
+            </div> 
 
           </div>
 
@@ -82,6 +95,25 @@
   </div>
 </template>
 
+<script>
+
+//import {required, minLength} from 'vuelidate/lib/validators'
+
+export default {
+  data() {
+    return {
+      medicamentos: {
+        apresentacao: '',
+        descricao: '',
+        principio: '',
+        empresa: ''
+      }
+    }
+  },
+
+
+}
+</script>
 
 <style scoped>
   .btn {
